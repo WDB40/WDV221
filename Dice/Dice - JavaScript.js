8033157f2ffdd25105ddc36bcb34d4ab3c23a039
WDB40 <wbrown1640@gmail.com>
2018-10-01 20:54:13 -0500

    End of Class

---
+++ Dice/Dice - JavaScript.js
@@ -0,0 +1,42 @@
+//---Global Vars---
+var PlayerName;
+var Wins = 0;
+//------------
+
+PlayerName = prompt("Please enter your name.", "John Doe");
+
+function getWins()
+{
+    document.getElementById("WinsSection").innerHTML = PlayerName + " has " + Wins + " wins.";
+}
+
+function formatSelectOption(inOption, inValue)
+{
+    return "<option value='" + inValue + "'>" + inOption + "</option>";
+}
+
+function getSelectedSettingOptionText()
+{
+    return document.getElementById("SettingsSelectList").selectedOptions[0].innerHTML;
+}
+
+function showSelectedDifficulty(){
+    document.getElementById("SettingsMessage").innerHTML = "Your selected difficulty: " + getSelectedSettingOptionText();
+}
+
+function changeImageSource(imageElementID, imageSource) {
+    document.getElementById(imageElementID).src = imageSource;
+}
+
+function getRandomDiceNumber(){
+    return Math.ceil(Math.random() * 6);
+}
+
+function getRandomDiceImage(){
+    return "imgs/" + getRandomDiceNumber() + ".PNG";
+}
+
+function rollTwoDice(imageElementID1, imageElementID2){
+    document.getElementById(imageElementID1).src = getRandomDiceImage();
+    document.getElementById(imageElementID2).src = getRandomDiceImage();
+}
---
+++ Unit 5/Unit 5 - JavaScript.js
@@ -0,0 +1,20 @@
+function completeProblem1(){
+
+    document.getElementById("problem1").innerHTML = "Problem 1 complete."
+}
+
+function completeProblem2() {
+    document.getElementById("problem2").innerHTML = "You did it. Problem 2 complete."
+}
+
+function completeProblem3() {
+    document.getElementById("problem3").innerHTML = "Why'd you leave? Problem 3 complete."
+}
+
+function completeProblem4(){
+    document.getElementById("problem4").innerHTML = "Nice job there. Problem 4 complete."
+}
+
+function completeProblem5(){
+    alert("Did this work?")
+}
